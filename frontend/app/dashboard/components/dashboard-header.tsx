99251087752e58cd98ac9a8fe56cd8a6243834cb
florian-alb <florianalbora@gmail.com>
2026-01-20 08:49:18 +0100

    update gitignore and dashboard skeleton

--- frontend/app/dashboard/components/dashboard-header.tsx
+++ frontend/app/dashboard/components/dashboard-header.tsx
@@ -1,14 +1,5 @@
 "use client";
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function DashboardHeader() {
@@ -16,11 +7,11 @@ export default function DashboardHeader() {
     <header className="flex h-16 shrink-0 items-center gap-2">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <Separator
+        {/*<Separator
           orientation="vertical"
           className="mr-2 data-[orientation=vertical]:h-4"
-        />
-        <Breadcrumb>
+        />*/}
+        {/*<Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
               <BreadcrumbLink href="#">
@@ -32,7 +23,7 @@ export default function DashboardHeader() {
               <BreadcrumbPage>Data Fetching</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
-        </Breadcrumb>
+        </Breadcrumb>*/}
       </div>
     </header>
   );
